290d798af5ea4efd98d82e56f21d3ed900b176f5
andrei <112718960+jekeim@users.noreply.github.com>
2023-11-11 22:35:11 +0200

    ai generated code is bad

--- src/install_ffmpeg.py
+++ src/install_ffmpeg.py
@@ -7,7 +7,7 @@ import zipfile
 def is_ffmpeg_installed():
     try:
         # Run the command to check if ffmpeg is installed
-        subprocess.run(['ffmpeg', '-version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+        subprocess.run(['ffmpeg', '-version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True, shell=True)
         return True
     except subprocess.CalledProcessError:
         return False
@@ -27,29 +27,15 @@ def install_ffmpeg(install_dir):
         download_path = 'ffmpeg.zip'
 
         # Download ffmpeg zip file
-        response = requests.get(download_url)
-        with open(download_path, 'wb') as zip_file:
-            zip_file.write(response.content)
+        #wip
 
-        # Extract the zip file
-        with zipfile.ZipFile(download_path, 'r') as zip_ref:
-            zip_ref.extractall(install_dir)
-
-        # Add ffmpeg directory to PATH
-        os.environ['PATH'] += os.pathsep + os.path.join(install_dir, 'bin')
-
+if is_ffmpeg_installed():
+    print("ffmpeg is already installed.")
+else:
+    install_dir = input("Enter the installation directory for ffmpeg (e.g., C:\\\\ffmpeg): ")
+    print("ffmpeg not found. Attempting to install...")
+    install_ffmpeg(install_dir)
+    if is_ffmpeg_installed():
         print("ffmpeg installation successful.")
     else:
-        print("Unsupported operating system. Please install ffmpeg manually.")
-
-if __name__ == "__main__":
-    if is_ffmpeg_installed():
-        print("ffmpeg is already installed.")
-    else:
-        install_dir = input("Enter the installation directory for ffmpeg (e.g., C:\\ffmpeg): ")
-        print("ffmpeg not found. Attempting to install...")
-        install_ffmpeg(install_dir)
-        if is_ffmpeg_installed():
-            print("ffmpeg installation successful.")
-        else:
-            print("Failed to install ffmpeg. Please install it manually.")
+        print("Failed to install ffmpeg. Please install it manually.")
